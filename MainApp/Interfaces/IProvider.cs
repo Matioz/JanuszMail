ca4e8d8e17a856f1cd94a9ccf3c51dec3eefb364
Mateusz Jablonski <matjabl1@student.pg.gda.pl>
2017-11-04 14:28:06 +0100

    Change provider response status type HttpResponse->HttpStatusCode

--- MainApp/Interfaces/IProvider.cs
+++ MainApp/Interfaces/IProvider.cs
@@ -1,23 +1,23 @@
 using System;
 using System.Collections.Generic;
+using System.Net;
 using System.Net.Mail;
 using JanuszMail.Models;
-using Microsoft.AspNetCore.Http;
 
 namespace JanuszMail.Interfaces
 {
     public interface IProvider
     {
         bool IsAuthenticated();
-        Tuple<IList<MailMessage>, HttpResponse> GetMailsFromFolder(string folder, int page, int pageSize);
-        Tuple<IList<string>, HttpResponse> GetFolders();
-        Tuple<IList<string>, HttpResponse> GetSubjectsFromFolder(string folder, int page, int pageSize);
-        HttpResponse SendEmail(MailMessage mailMessage);
-        HttpResponse RemoveEmail(MailMessage mailMessage);
-        HttpResponse MoveEmailToFolder(MailMessage mailMessage, string folder);
-        HttpResponse MarkEmailAsRead(MailMessage mailMessage);
-        HttpResponse MarkEmailAsUnread(MailMessage mailMessage);
-        HttpResponse Connect(ProviderParams providerParams);
-        HttpResponse Disconnect(ProviderParams providerParams);
+        Tuple<IList<MailMessage>, HttpStatusCode> GetMailsFromFolder(string folder, int page, int pageSize);
+        Tuple<IList<string>, HttpStatusCode> GetFolders();
+        Tuple<IList<string>, HttpStatusCode> GetSubjectsFromFolder(string folder, int page, int pageSize);
+        HttpStatusCode SendEmail(MailMessage mailMessage);
+        HttpStatusCode RemoveEmail(MailMessage mailMessage);
+        HttpStatusCode MoveEmailToFolder(MailMessage mailMessage, string folder);
+        HttpStatusCode MarkEmailAsRead(MailMessage mailMessage);
+        HttpStatusCode MarkEmailAsUnread(MailMessage mailMessage);
+        HttpStatusCode Connect(ProviderParams providerParams);
+        HttpStatusCode Disconnect(ProviderParams providerParams);
     }
 }

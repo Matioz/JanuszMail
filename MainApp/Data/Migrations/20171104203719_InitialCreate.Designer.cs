0a565405869f3611f76849edcc7ef68832838fda
Mateusz Jablonski <matjabl1@student.pg.gda.pl>
2017-11-04 21:40:49 +0100

    Some major changes:
    - Change type of User handle in ProviderParams: ApplicationUser -> string
      Example in MailBoxController is updated
    - Remove local database from repository, added to .gitignore
    
    To create database properly you should type following commands in repo root:
    "cd MainApp"
    "dotnet restore"
    "dotnet ef database update -c ApplicationDbContext"
    "dotnet ef database update -c JanuszMailDbContext"

--- MainApp/Data/Migrations/20171104203719_InitialCreate.Designer.cs
+++ MainApp/Data/Migrations/20171104203719_InitialCreate.Designer.cs
@@ -10,9 +10,10 @@ using System;
 namespace JanuszMail.Data.Migrations
 {
     [DbContext(typeof(JanuszMailDbContext))]
-    partial class JanuszMailDbContextModelSnapshot : ModelSnapshot
+    [Migration("20171104203719_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

19d6ec2deebd0cc9abb3966be23596adf747f14a
Mateusz Jablonski <matjabl1@student.pg.gda.pl>
2017-11-08 18:42:25 +0100

    Some minor fixes making this project works

--- MainApp/Services/Provider.cs
+++ MainApp/Services/Provider.cs
@@ -15,224 +15,267 @@ namespace JanuszMail.Services
     public class Provider : IProvider
     {
         //TODO: Maciej Plewka should fill these implementations
-        private ImapClient _imapClient {get;set;}
-        private MailKit.Net.Smtp.SmtpClient _smtpClient {get;set;}
+        private ImapClient _imapClient;
+        private MailKit.Net.Smtp.SmtpClient _smtpClient;
         public HttpStatusCode Connect(ProviderParams providerParams)
         {
             _imapClient = new ImapClient();
             _smtpClient = new MailKit.Net.Smtp.SmtpClient();
-            _imapClient.ServerCertificateValidationCallback = (s,c,h,e) => true;
-            try{
-                _imapClient.Connect (providerParams.ImapServerName, providerParams.ImapPortNumber, true);
+            _imapClient.ServerCertificateValidationCallback = (s, c, h, e) => true;
+            try
+            {
+                _imapClient.Connect(providerParams.ImapServerName, providerParams.ImapPortNumber, true);
+                _smtpClient.Connect(providerParams.SmtpServerName, providerParams.SmtpPortNumber, true);
+                _imapClient.AuthenticationMechanisms.Remove("XOAUTH2");
+                _smtpClient.Authenticate(providerParams.EmailAdress, providerParams.Password);
+                _imapClient.Authenticate(providerParams.EmailAdress, providerParams.Password);
             }
-            catch(Exception ex){
-                return HttpStatusCode.ExpectationFailed;
-            }
-            try{
-                _smtpClient.Connect (providerParams.SmtpServerName, providerParams.SmtpPortNumber, true);
-            }
-            catch(Exception ex){
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.ToString());
                 return HttpStatusCode.ExpectationFailed;
             }
 
-            _imapClient.AuthenticationMechanisms.Remove ("XOAUTH2");
-
-            try{
-                _smtpClient.Authenticate (providerParams.EmailAdress, providerParams.Password);
+            if (_imapClient.IsConnected && _smtpClient.IsConnected)
+            {
+                return HttpStatusCode.OK;
             }
-            catch(Exception ex){
-                return HttpStatusCode.ExpectationFailed;
-            }
-            try{
-                _imapClient.Authenticate (providerParams.EmailAdress, providerParams.Password);
-            }
-            catch(Exception ex){
-                return HttpStatusCode.ExpectationFailed;
-            }
-            if(_imapClient.IsConnected && _smtpClient.IsConnected){
-                return HttpStatusCode.Created;
-            }
-            else{
-               return HttpStatusCode.InternalServerError;
+            else
+            {
+                return HttpStatusCode.InternalServerError;
             }
         }
 
         public HttpStatusCode Disconnect(ProviderParams providerParams)
         {
-            if(_imapClient.IsConnected && _smtpClient.IsConnected){
-                try{
+            if (IsConnected())
+            {
+                try
+                {
                     _imapClient.Disconnect(true);
-                 }
-                catch(Exception ex){
-                    return HttpStatusCode.ExpectationFailed;
-                }
-                try{
                     _smtpClient.Disconnect(true);
                 }
-                catch(Exception ex){
+                catch (Exception exception)
+                {
+                    Console.WriteLine(exception.ToString());
                     return HttpStatusCode.ExpectationFailed;
                 }
-                if(_imapClient.IsConnected && _smtpClient.IsConnected){
+                if (_imapClient.IsConnected && _smtpClient.IsConnected)
+                {
                     return HttpStatusCode.InternalServerError;
                 }
-                return HttpStatusCode.Created;
+                return HttpStatusCode.OK;
             }
-            else{
+            else
+            {
                 return HttpStatusCode.InternalServerError;
             }
         }
         public Tuple<IList<string>, HttpStatusCode> GetFolders()
         {
-            IList<string> Folders =new List<string>();
-            if(!_imapClient.IsConnected){
-                return new Tuple<IList<string>, HttpStatusCode>(Folders, HttpStatusCode.InternalServerError);
+            IList<string> Folders = new List<string>();
+            if (!IsAuthenticated())
+            {
+                return new Tuple<IList<string>, HttpStatusCode>(Folders, HttpStatusCode.ExpectationFailed);
             }
-            var personal = _imapClient.GetFolder (_imapClient.PersonalNamespaces[0]);
-                foreach (var folder in personal.GetSubfolders (false)){
-                    foreach (var subfolder in folder.GetSubfolders(false)){
-	                    Folders.Add(subfolder.Name);
-                    }
-                    Folders.Add(folder.Name);
+            var personal = _imapClient.GetFolder(_imapClient.PersonalNamespaces[0]);
+            foreach (var folder in personal.GetSubfolders(false))
+            {
+                foreach (var subfolder in folder.GetSubfolders(false))
+                {
+                    Folders.Add(subfolder.Name);
                 }
-            return new Tuple<IList<string>, HttpStatusCode>(Folders, HttpStatusCode.Created);
+                Folders.Add(folder.Name);
+            }
+            return new Tuple<IList<string>, HttpStatusCode>(Folders, HttpStatusCode.OK);
         }
 
         public Tuple<IList<MimeMessage>, HttpStatusCode> GetMailsFromFolder(string folder, int page, int pageSize)
         {
-            if(!_imapClient.IsConnected){
-                return new Tuple<IList<MimeMessage>, HttpStatusCode>(null, HttpStatusCode.InternalServerError);
+            if (!IsAuthenticated())
+            {
+                return new Tuple<IList<MimeMessage>, HttpStatusCode>(null, HttpStatusCode.ExpectationFailed);
             }
-            IMailFolder mailFolder=getFolder(folder);
-            mailFolder.Open (FolderAccess.ReadWrite);
+            IMailFolder mailFolder = GetFolder(folder);
+            mailFolder.Open(FolderAccess.ReadWrite);
             IList<MimeMessage> Mails = new List<MimeMessage>();
-            for (int i = page*pageSize; i < mailFolder.Count && i < pageSize; i++) 
-                    {
-                        var message = mailFolder.GetMessage (i);
-                        Mails.Insert(0, message);
-                    }
+            for (int i = (page - 1) * pageSize; i < mailFolder.Count && i < pageSize; i++)
+            {
+                var message = mailFolder.GetMessage(i);
+                Mails.Insert(0, message);
+            }
             mailFolder.Close();
-            return new Tuple<IList<MimeMessage>, HttpStatusCode>(Mails, HttpStatusCode.Created);
+            return new Tuple<IList<MimeMessage>, HttpStatusCode>(Mails, HttpStatusCode.OK);
         }
 
         public Tuple<IList<string>, HttpStatusCode> GetSubjectsFromFolder(string folder, int page, int pageSize)
         {
-            if(!_imapClient.IsConnected){
-                return new Tuple<IList<string>, HttpStatusCode>(null, HttpStatusCode.InternalServerError);
+            if (!IsAuthenticated())
+            {
+                return new Tuple<IList<string>, HttpStatusCode>(null, HttpStatusCode.ExpectationFailed);
             }
-            IMailFolder mailFolder=getFolder(folder);
-            if(mailFolder == null){
+            IMailFolder mailFolder = GetFolder(folder);
+            if (mailFolder == null)
+            {
                 return new Tuple<IList<string>, HttpStatusCode>(new List<string>(), HttpStatusCode.InternalServerError);
             }
-            mailFolder.Open (FolderAccess.ReadWrite);
+            mailFolder.Open(FolderAccess.ReadWrite);
             IList<string> Subjects = new List<string>();
-            for (int i = page*pageSize; i < mailFolder.Count && i < pageSize; i++) 
-                    {
-                        var message = mailFolder.GetMessage (i);
-                        Subjects.Insert(0, message.Subject);
-                    }
+            for (int i = page * pageSize; i < mailFolder.Count && i < pageSize; i++)
+            {
+                var message = mailFolder.GetMessage(i);
+                Subjects.Insert(0, message.Subject);
+            }
             mailFolder.Close();
-            return new Tuple<IList<string>, HttpStatusCode>(Subjects, HttpStatusCode.Created);
+            return new Tuple<IList<string>, HttpStatusCode>(Subjects, HttpStatusCode.OK);
         }
 
         public bool IsAuthenticated()
         {
-            return _imapClient.IsAuthenticated;
+
+            return IsConnected() && _imapClient.IsAuthenticated && _smtpClient.IsAuthenticated;
         }
 
         public HttpStatusCode MarkEmailAsRead(MimeMessage mailMessage, string folder)
         {
-            IMailFolder mailFolder=getFolder(folder);
-            if(folder!=null){
-                mailFolder.Open (FolderAccess.ReadWrite);
-                mailFolder.AddFlags(getUniqueId(mailMessage, folder), MessageFlags.Seen, true);
-                mailFolder.Close();
-                return HttpStatusCode.Created;
+            if (!IsAuthenticated())
+            {
+                return HttpStatusCode.ExpectationFailed;
             }
-            else{
+            IMailFolder mailFolder = GetFolder(folder);
+            if (folder != null)
+            {
+                mailFolder.Open(FolderAccess.ReadWrite);
+                mailFolder.AddFlags(GetUniqueId(mailMessage, folder), MessageFlags.Seen, true);
+                mailFolder.Close();
+                return HttpStatusCode.OK;
+            }
+            else
+            {
                 return HttpStatusCode.InternalServerError;
             }
         }
 
         public HttpStatusCode MarkEmailAsUnread(MimeMessage mailMessage, string folder)
         {
-            IMailFolder mailFolder=getFolder(folder);
-            if(mailFolder != null){
-                mailFolder.Open (FolderAccess.ReadWrite);
-                mailFolder.RemoveFlags(getUniqueId(mailMessage, folder), MessageFlags.Seen, true);
-                mailFolder.Close();
-                return HttpStatusCode.Created;
+            if (!IsAuthenticated())
+            {
+                return HttpStatusCode.ExpectationFailed;
             }
-            else{
+            IMailFolder mailFolder = GetFolder(folder);
+            if (mailFolder != null)
+            {
+                mailFolder.Open(FolderAccess.ReadWrite);
+                mailFolder.RemoveFlags(GetUniqueId(mailMessage, folder), MessageFlags.Seen, true);
+                mailFolder.Close();
+                return HttpStatusCode.OK;
+            }
+            else
+            {
                 return HttpStatusCode.InternalServerError;
             }
         }
 
         public HttpStatusCode MoveEmailToFolder(MimeMessage mailMessage, string folderSrc, string folderDst)
         {
-            IMailFolder mailFolder=getFolder(folderSrc);
-            if(mailFolder != null){
-            mailFolder.Open (FolderAccess.ReadWrite);
-            mailFolder.MoveTo(getUniqueId(mailMessage, folderSrc), getFolder(folderDst));
-            return HttpStatusCode.Created;
+            if (!IsAuthenticated())
+            {
+                return HttpStatusCode.ExpectationFailed;
             }
-            else{
+            IMailFolder mailFolder = GetFolder(folderSrc);
+            if (mailFolder != null)
+            {
+                mailFolder.Open(FolderAccess.ReadWrite);
+                mailFolder.MoveTo(GetUniqueId(mailMessage, folderSrc), GetFolder(folderDst));
+                return HttpStatusCode.OK;
+            }
+            else
+            {
                 return HttpStatusCode.InternalServerError;
             }
         }
 
         public HttpStatusCode RemoveEmail(MimeMessage mailMessage, string folder)
         {
-            IMailFolder mailFolder=getFolder(folder);
-            if(mailFolder != null){
-                mailFolder.Open (FolderAccess.ReadWrite);
-                mailFolder.AddFlags(getUniqueId(mailMessage, folder), MessageFlags.Deleted, true);
+            if (!IsAuthenticated())
+            {
+                return HttpStatusCode.ExpectationFailed;
+            }
+            IMailFolder mailFolder = GetFolder(folder);
+            if (mailFolder != null)
+            {
+                mailFolder.Open(FolderAccess.ReadWrite);
+                mailFolder.AddFlags(GetUniqueId(mailMessage, folder), MessageFlags.Deleted, true);
                 mailFolder.Expunge();
                 mailFolder.Close();
-                return HttpStatusCode.Created;
+                return HttpStatusCode.OK;
             }
-            else{
+            else
+            {
                 return HttpStatusCode.InternalServerError;
             }
         }
 
         public HttpStatusCode SendEmail(MimeMessage mailMessage)
         {
+            if (!IsAuthenticated())
+            {
+                return HttpStatusCode.ExpectationFailed;
+            }
             _smtpClient.Send(mailMessage);
-            return HttpStatusCode.Created;
+            return HttpStatusCode.OK;
         }
-        public IMailFolder getFolder(string name){
-            IMailFolder mailFolder=null;
-            var personal = _imapClient.GetFolder (_imapClient.PersonalNamespaces[0]);
-                foreach (var folder in personal.GetSubfolders (false)){
-                    foreach (var subfolder in folder.GetSubfolders(false)){
-	                    if(subfolder.Name == name){
-                            mailFolder = subfolder;
-                        }
+        private IMailFolder GetFolder(string name)
+        {
+            IMailFolder mailFolder = null;
+            var personal = _imapClient.GetFolder(_imapClient.PersonalNamespaces[0]);
+            foreach (var folder in personal.GetSubfolders(false))
+            {
+                foreach (var subfolder in folder.GetSubfolders(false))
+                {
+                    if (subfolder.Name == name)
+                    {
+                        mailFolder = subfolder;
                     }
-                    if(folder.Name == name){
-                            mailFolder = folder;
-                        }
                 }
+                if (folder.Name == name)
+                {
+                    mailFolder = folder;
+                }
+            }
             return mailFolder;
         }
-        public UniqueId getUniqueId(MimeMessage mailMessage, string folder){
-            UniqueId id= new UniqueId();
-            IMailFolder mailFolder=getFolder(folder);
+
+        private UniqueId GetUniqueId(MimeMessage mailMessage, string folder)
+        {
+            UniqueId id = new UniqueId();
+            IMailFolder mailFolder = GetFolder(folder);
             bool open = mailFolder.IsOpen;
-            if(!open){
-                mailFolder.Open (FolderAccess.ReadOnly);
+            if (!open)
+            {
+                mailFolder.Open(FolderAccess.ReadOnly);
             }
-            var items = mailFolder.Fetch (0, -1, MessageSummaryItems.UniqueId | MessageSummaryItems.Size | MessageSummaryItems.Flags);
-            foreach (var item in items) {
-                var message = mailFolder.GetMessage (item.UniqueId);
-                if(message.MessageId == mailMessage.MessageId){
+            var items = mailFolder.Fetch(0, -1, MessageSummaryItems.UniqueId | MessageSummaryItems.Size | MessageSummaryItems.Flags);
+            foreach (var item in items)
+            {
+                var message = mailFolder.GetMessage(item.UniqueId);
+                if (message.MessageId == mailMessage.MessageId)
+                {
                     id = item.UniqueId;
                     break;
                 }
-            }if(!open){
-                mailFolder.Close ();
+            }
+            if (!open)
+            {
+                mailFolder.Close();
             }
             return id;
+        }
+
+        private bool IsConnected()
+        {
+
+            return _imapClient != null && _imapClient.IsConnected && _smtpClient != null && _smtpClient.IsConnected;
         }
     }
 }

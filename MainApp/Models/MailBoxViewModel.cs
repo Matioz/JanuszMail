b3dcc365382947a013532261af6d336309e74baa
andrzejDudax3 <maciekplewka@wp.pl>
2017-11-08 11:51:50 +0100

    Improve provider methods

---
+++ MainApp/Models/MailBoxViewModel.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+
+namespace JanuszMail.Models
+{
+    public class MailBoxViewModel
+    {
+        public IList<string> Subjects;
+        public IList<string> Folders;
+    }
+}

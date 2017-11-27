d3b0bad16b071d530516ac6367c2bd20fd50f708
Mateusz Jablonski <matjabl1@student.pg.gda.pl>
2017-11-27 19:33:08 +0100

    Fix sending mails with attachments

--- MainApp/wwwroot/js/Mailbox/send.js
+++ MainApp/wwwroot/js/Mailbox/send.js
@@ -6,10 +6,23 @@ $(".async-form").submit(function (event) {
     $("#modalButton").click();
     event.preventDefault();
     var url = $(this).attr("action") + "/" + actionName;
+
+    var fd = new FormData();
+    fd.append('__RequestVerificationToken', $('[name=__RequestVerificationToken]').val());
+    fd.append('Subject', $('[name=Subject]').val());
+    fd.append('Recipient', $('[name=Recipient]').val());
+    fd.append('Body', $('[name=Body]').val());
+    fd.append('ID', $('[name=ID]').val());
+    var file_data = $('input[type="file"]')[0].files; // for multiple files
+    for (var i = 0; i < file_data.length; i++) {
+        fd.append("Attachments", file_data[i]);
+    }
     $.ajax({
         url: url,
         type: 'POST',
-        data: $(this).serialize(),
+        data: fd,
+        processData: false,
+        contentType: false,
         ajaxSend: $('#statusBody').html('<div class="text-center block">Please wait <img src="images/ajax-loader.gif" alt="Processing"/></div>'),
         success:
             function (data) {

80f0b4eb8c3e9ce5de1d2595dd2d46b8336ec823
Grzegorz Beringer <grzegorz.beringer@gmail.com>
2017-11-28 22:35:34 +0100

    remove unused template files

--- MainApp/Controllers/AccountController.cs
+++ MainApp/Controllers/AccountController.cs
@@ -247,7 +247,7 @@ namespace JanuszMail.Controllers
         {
             await _signInManager.SignOutAsync();
             _logger.LogInformation("User logged out.");
-            return RedirectToAction(nameof(HomeController.Index), "Home");
+            return RedirectToAction(nameof(MailBoxController.Index), "");
         }
 
         [HttpPost]
@@ -335,7 +335,7 @@ namespace JanuszMail.Controllers
         {
             if (userId == null || code == null)
             {
-                return RedirectToAction(nameof(HomeController.Index), "Home");
+                return RedirectToAction(nameof(MailBoxController.Index), "");
             }
             var user = await _userManager.FindByIdAsync(userId);
             if (user == null)
@@ -455,7 +455,7 @@ namespace JanuszMail.Controllers
             }
             else
             {
-                return RedirectToAction(nameof(HomeController.Index), "Home");
+                return RedirectToAction(nameof(MailBoxController.Index), "");
             }
         }
 
